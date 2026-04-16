b7394bb24c994a153a1a822b98dfe74972001a59
MohamedKhaled8 <mohamedkhaled9002432@gmail.com>
2026-04-16 23:54:10 +0200

    Update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPgWl2GPpMfCCsHhDdJ6YpgxxDgvzi73w",
-  authDomain: "alkema-31f95.firebaseapp.com",
-  projectId: "alkema-31f95",
-  storageBucket: "alkema-31f95.firebasestorage.app",
-  messagingSenderId: "329074550673",
-  appId: "1:329074550673:web:bd7eec254663e73c981c30",
-  measurementId: "G-LSYSQYWJQ9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
